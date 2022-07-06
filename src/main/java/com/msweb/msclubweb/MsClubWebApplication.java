6646b9e93615b0a5232c8b6a91672ae9157373c7
jinwei <1969808824@qq.com>
2022-07-06 12:19:41 +0800

    Init:SpringBoot Project

---
+++ src/main/java/com/msweb/msclubweb/MsClubWebApplication.java
@@ -0,0 +1,13 @@
+package com.msweb.msclubweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsClubWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsClubWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msweb/msclubweb/MsClubWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msweb.msclubweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClubWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
